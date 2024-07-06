afaa40ed9afb5c49278fa08ce3fdf74cb9ef7f9f
Jacob Cabot <166416094+JacobBCabot@users.noreply.github.com>
2024-07-06 17:03:42 +0000

    2.0

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,3 +1,5 @@
+import { json } from "react-router";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
@@ -17,7 +19,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 		},
 		actions: {
 			getContacts:() => {
-				fetch("https://playground.4geeks.com/contact/docs")
+				fetch("https://playground.4geeks.com/contact/agendas/JacobBCabot")
 				.then((resp) => {
 					if(!resp.ok) throw Error(resp.statusText);
 					return resp.json();
@@ -30,8 +32,63 @@ const getState = ({ getStore, getActions, setStore }) => {
 				.catch((error) => {
 					console.log(error);
 				})
-			}
-			}
+			},
+			addContacts: (contactData) => {
+				const url = "https://playground.4geeks.com/apis/fake/contact/";
+				const request = {
+					method:"POST",
+					headers:{"Content-Type":"application/json"},
+					body:JSON.stringify(contactData),
+				};
+				fetch(url, request)
+				.then((resp) => {
+					if(!resp.ok) throw Error(resp.statusText);
+					return resp.json();
+				})
+				.then((data) =>{
+					console.log(data);
+					getActions().getContacts();
+
+				})
+				.catch((error) =>{
+					console.log(error);
+
+				})
+			},
+			deleteContacts: (id) => {
+				const url = "https://playground.4geeks.com/apis/fake/contact/${id}";
+				const request = {
+					method:"DELETE",
+					headers:{"Content-Type":"application/json"},
+				};
+				fetch(url, request)
+				.then((resp) => {
+					if(!resp.ok) throw Error(resp.statusText);
+					return resp.json();
+				})
+				.then((data) =>{
+					console.log(data);
+					getActions().getContacts();
+
+				})
+				.catch((error) =>{
+					console.log(error);
+
+				});
+
+
+			},
+			editContact:(id, contactData) => {
+				fetch("https://playground.4geeks.com/apis/fake/contact/${id}",{
+					method:"PUT",
+					headers:{"Content-Type":"application/json",
+
+					},
+					body:JSON.stringify(contactData),
+				})
+				
+				
+			} 
 		}
 	};
 };
